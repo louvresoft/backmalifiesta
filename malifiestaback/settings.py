10b5d4c7b4d9b600c28abfda2b20c9db26318564
marco_romeromon <marco.romero@eon.com.mx>
2021-12-14 13:44:11 -0600

    se agregan corsheaders

--- malifiestaback/settings.py
+++ malifiestaback/settings.py
@@ -157,6 +157,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8002",
     "http://127.0.0.1:4200",
     "http://localhost:4200",
+    "https://malifiestas.web.app/",
+    "https://malifiestas.web.app"
 ]
 
 CORS_ALLOWED_ORIGINS_REGEX = [
