9bbac389822b08c42f15d8fef386fea4ccafd590
marco_romeromon <marco.romero@eon.com.mx>
2021-12-14 18:47:01 -0600

    se pagina productos

--- apps/cat_wms/api/views/producto_view.py
+++ apps/cat_wms/api/views/producto_view.py
@@ -1,5 +1,5 @@
 
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -8,13 +8,10 @@ from apps.cat_wms.api import serializers as srlzrs
 
 class ProductoViewSet(viewsets.ModelViewSet):
     serializer_class = srlzrs.ProductoSerializer
+    pagination_class = pagination.LimitOffsetPagination
 
     def get_queryset(self, pk=None):
         if pk is None:
             return self.get_serializer().Meta.model.objects.filter(state=True)
         else:
             return self.get_serializer().Meta.model.objects.filter(state=True, id=pk).first()
-
-    def list(self, request):
-        product_serializer = self.get_serializer(self.get_queryset(), many=True)
-        return Response(product_serializer.data, status=status.HTTP_200_OK)
